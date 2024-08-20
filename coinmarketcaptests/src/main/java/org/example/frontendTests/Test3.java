f9d37689792176dda60fb74a76fe6ddc0ce32002
nikolamladenovicc <mladenovicn68@gmail.com>
2024-08-20 15:59:40 +0200

    Started refactoring FE Test 2.

--- coinmarketcaptests/src/main/java/org/example/frontendTests/Test3.java
+++ coinmarketcaptests/src/main/java/org/example/frontendTests/Test3.java
@@ -1,5 +1,6 @@
 package org.example.frontendTests;
 
+import org.example.pageObjectModels.MainPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,12 +15,14 @@ import java.util.List;
 
 public class Test3 {
     private WebDriver driver;
+    private MainPage mainPage;
 
     @BeforeClass
     public void setUp() {
         // Set the path of the ChromeDriver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\QH0158\\IdeaProjects\\chromedriver\\chromedriver.exe");
         driver = new ChromeDriver();
+        mainPage = new MainPage(driver);
     }
 
     @Test
@@ -29,50 +32,26 @@ public class Test3 {
         Thread.sleep(5000);
 
         // Click the drop-down menu on "Cryptocurrencies" tab
-        WebElement cryptoTab = driver.findElement(By.cssSelector("#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.sc-2e66506f-0.jztNxX > div.sc-d8ea16bb-0.hZhywa > div:nth-child(4) > div > div.sc-2fe50849-0.iqytLW > div:nth-child(3) > div"));
-        cryptoTab.click();
+        mainPage.openCryptoTab();
         Thread.sleep(2000);
-
-        WebElement currenciesTab = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[1]/div[2]/div[4]/div/div[3]/div[2]/section/div[1]/div[1]"));
-        currenciesTab.click();
+        mainPage.selectCurrenciesTab();
         Thread.sleep(2000);
-
-        WebElement fullListOption = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[1]/div[2]/div[4]/div/div[3]/div[2]/section/div[1]/div[2]/div/div[1]/div[2]/a[1]"));
-        fullListOption.click();
+        mainPage.selectFullListOption();
         Thread.sleep(5000);
 
         // Record the data from the current page
-        List<WebElement> initialData = driver.findElements(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[4]/table"));
-        List<String> initialCryptoNames = new ArrayList<>();
-        for (WebElement crypto : initialData) {
-            initialCryptoNames.add(crypto.getText());
-        }
+        List<String> initialData = mainPage.recordInitialData();
 
         // Use the filters
-        WebElement filterButton = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div[2]/div/button[1]"));
-        filterButton.click();
-        Thread.sleep(2000);
-
-        WebElement categoryButton = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/ul/li[1]/div/span/button"));
-        categoryButton.click();
-        Thread.sleep(2000);
-
-        WebElement platformButton = driver.findElement(By.className("sc-82271cd3-2"));
-        platformButton.click();
-        Thread.sleep(2000);
+        mainPage.applyFilters();
 
         // Record the filtered data from page
-        List<WebElement> filteredData = driver.findElements(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[4]/table"));
-        List<String> filteredCryptoNames = new ArrayList<>();
-        for (WebElement crypto : filteredData) {
-            filteredCryptoNames.add(crypto.getText());
-        }
+        List<String> filteredData = mainPage.recordFilteredData();
 
         // Check if the all filtered data are different from the initial data
-        Assert.assertNotEquals(filteredCryptoNames, initialCryptoNames, "TEST FAILED | Filtered data are the same as the initial data.");
-
+        Assert.assertNotEquals(filteredData, initialData, "TEST FAILED | Filtered data are the same as the initial data.");
         // Check the filtered data against the initial data
-        if (initialCryptoNames == filteredCryptoNames){
+        if (initialData.equals(filteredData)){
             System.out.println("TEST FAILED | The both initial and filtered data are the same.");
         }
         else {
--- coinmarketcaptests/src/main/java/org/example/pageObjectModels/MainPage.java
+++ coinmarketcaptests/src/main/java/org/example/pageObjectModels/MainPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainPage {
@@ -17,10 +18,16 @@ public class MainPage {
     // Locators
     private By viewAllButton = By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div[1]/a[1]/button");
     private By resultsTable = By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[4]/table");
-
     private By cryptoRowsLocator = By.cssSelector("tbody tr");
-
     private By watchlistStarIconLocator = By.className("icon-Star");
+    private By cryptoTab = By.cssSelector("#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.sc-2e66506f-0.jztNxX > div.sc-d8ea16bb-0.hZhywa > div:nth-child(4) > div > div.sc-2fe50849-0.iqytLW > div:nth-child(3) > div");
+    private By currenciesTab = By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[1]/div[2]/div[4]/div/div[3]/div[2]/section/div[1]/div[1]");
+    private By fullListOption = By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[1]/div[2]/div[4]/div/div[3]/div[2]/section/div[1]/div[2]/div/div[1]/div[2]/a[1]");
+    private By cryptoTable = By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[4]/table");
+    private By filterButton = By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div[2]/div/button[1]");
+    private By categoryButton = By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/ul/li[1]/div/span/button");
+    private By platformButton = By.className("sc-82271cd3-2");
+
     // Constructors
     public MainPage(WebDriver driver){
         this.driver = driver;
@@ -51,6 +58,53 @@ public class MainPage {
         WebElement nameElement = cryptoRow.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div/div[1]/div[4]/table/tbody/tr[1]/td[3]/div/a/div/div/div/p"));
         return nameElement.getText();
     }
+
+    public void openCryptoTab(){
+        driver.findElement(cryptoTab).click();
+    }
+
+    public void selectCurrenciesTab(){
+        driver.findElement(currenciesTab).click();
+    }
+
+    public void selectFullListOption(){
+        driver.findElement(fullListOption).click();
+    }
+    public List<String> recordInitialData() {
+        List<WebElement> initialData = driver.findElements(cryptoTable);
+        List<String> initialCryptoNames = new ArrayList<>();
+        for (WebElement crypto : initialData) {
+            initialCryptoNames.add(crypto.getText());
+        }
+        return initialCryptoNames;
+    }
+    public void applyFilters() throws InterruptedException {
+        driver.findElement(filterButton).click();
+        Thread.sleep(2000);
+        driver.findElement(categoryButton).click();
+        Thread.sleep(2000);
+        driver.findElement(platformButton).click();
+        Thread.sleep(2000);
+    }
+    public void filterButton(){
+        driver.findElement(filterButton).click();
+    }
+    public void categoryButton(){
+        driver.findElement(categoryButton).click();
+    }
+    public void platformButton(){
+        driver.findElement(platformButton).click();
+    }
+    public List<String> recordFilteredData() {
+        List<WebElement> filteredData = driver.findElements(cryptoTable);
+        List<String> filteredCryptoNames = new ArrayList<>();
+        for (WebElement crypto : filteredData) {
+            filteredCryptoNames.add(crypto.getText());
+        }
+        return filteredCryptoNames;
+    }
+
+
 
 
 }
